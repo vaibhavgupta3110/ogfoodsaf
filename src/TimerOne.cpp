d1203777c573d90033bc0c07d354da526ec207a2
vaibhavgupta3110 <vaibhav.gupta@me.com>
2017-01-11 18:31:57 +0530

    Update TimerOne.cpp
    change clock bit

--- src/TimerOne.cpp
+++ src/TimerOne.cpp
@@ -19,7 +19,7 @@
 TimerOne Timer1;              // preinstatiate
 
 unsigned short TimerOne::pwmPeriod = 0;
-unsigned char TimerOne::clockSelectBits = 0;
+unsigned char TimerOne::clockSelectBits = 16;
 void (*TimerOne::isrCallback)() = NULL;
 
 // interrupt service routine that wraps a user defined function supplied by attachInterrupt
